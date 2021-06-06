ef527ec50488ba11e0130136fe20331205260438
Pragatimanoorkar <pragatimanoorkar1128@gmail.com>
2021-06-06 15:22:58 +0530

    project setup

---
+++ java.js
@@ -0,0 +1,8 @@
+b = document.querySelector("button")
+a = document.qureySelector("text")
+c =  document.querySelector(".main")
+  b.addEventListner("click", showMessage),
+  function showMessage(){
+    
+ document.getElementById("main").innerHtml = "Welcome To Java Script"
+  }
